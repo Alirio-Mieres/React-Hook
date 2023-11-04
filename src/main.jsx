9bcf789ae3eb2ac64ae1e37aaebbcc567a6ea07c
Alirio Mieres <mie21001@byui.edu>
2023-11-04 14:54:20 -0400

    Refactor main.jsx to import FormWithCustomHook and comment out unused components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+// import { CounterApp } from './01-useState/CounterApp';
+// import { HooksApp } from './HooksApp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <HooksApp /> */}
+    {/* <CounterApp /> */}
+    {/* <CounterWithCustomHook /> */}
+    {/* <SimpleForm /> */}
+    <FormWithCustomHook />
   </React.StrictMode>,
 )
