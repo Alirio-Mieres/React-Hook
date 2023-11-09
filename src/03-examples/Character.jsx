2e708a98c89a226584765d8d9f009ad6484e6abd
Alirio Mieres <mie21001@byui.edu>
2023-11-09 10:02:40 -0400

    Add box size to Character component

--- src/03-examples/Character.jsx
+++ src/03-examples/Character.jsx
@@ -1,11 +1,31 @@
 import PropTypes from "prop-types";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export const Character = ({ species, name }) => {
+  const pRef = useRef();
+
+  const [boxSize, setBoxSize] = useState({
+    with: 0,
+    height: 0,
+  });
+
+  useLayoutEffect(() => {
+    const { height, width } = pRef.current.getBoundingClientRect();
+    setBoxSize({height, width});
+
+  }, [name]);
+
   return (
-    <blockquote className="blockquote text-end">
-      <p className="mb-1">{species}</p>
-      <footer className="blockquote-footer">{name}</footer>
-    </blockquote>
+    <>
+      <blockquote className="blockquote text-end" style={{ display: "flex" }}>
+        <p className="mb-1">{species}</p>
+        <footer ref={pRef} className="blockquote-footer">
+          {name}
+        </footer>
+      </blockquote>
+
+      <code>{  JSON.stringify(boxSize) }</code>
+    </>
   );
 };
 
