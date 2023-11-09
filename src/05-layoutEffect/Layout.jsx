51b4be79eb4832071f1f8916b068e5cee52db378
Alirio Mieres <mie21001@byui.edu>
2023-11-09 10:02:25 -0400

    Add Layout component with useCounter and useFetch hooks

---
+++ src/05-layoutEffect/Layout.jsx
@@ -0,0 +1,29 @@
+import { useCounter, useFetch } from "../hooks/";
+import { Character, LoadingCharacter  } from "../03-examples";
+
+export const Layout = () => {
+  const { counter, increment } = useCounter(1);
+
+  const { data, isLoading, hasError } = useFetch(
+    `https://rickandmortyapi.com/api/character/${counter}`
+  );
+
+  const { species, name } = !!data && data;
+
+  return (
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {isLoading ? <LoadingCharacter /> : <Character species={species} name={name} />}
+
+      <button
+        onClick={() => increment()}
+        disabled={isLoading}
+        className="btn btn-primary"
+      >
+        Next Character
+      </button>
+    </>
+  );
+};
