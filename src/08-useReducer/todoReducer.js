ad75f20d7de7c6f5bcacac10df6bfe45ec7d7aba
Alirio Mieres <mie21001@byui.edu>
2023-11-13 11:39:49 -0400

    Add remove todo functionality to todoReducer

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -2,6 +2,10 @@ export const todoReducer = (initialState = [], action) => {
   switch (action.type) {
     case "[TODO] add Todo":
       return [...initialState, action.payload];
+
+    case '[TODO] remove Todo':
+      return initialState.filter(todo => todo.id !== action.payload);
+    
     default:
       return initialState;
   }
