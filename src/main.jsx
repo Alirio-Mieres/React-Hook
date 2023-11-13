2adf3a9fe8619e36b9f5ea81c0bdf658d4aff8d8
Alirio Mieres <mie21001@byui.edu>
2023-11-13 12:30:43 -0400

    Refactor main.jsx to use TodoApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
-// import { SimpleForm } from './02-useEffect/SimpleForm';
-// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-// import { CounterApp } from './01-useState/CounterApp';
-// import { HooksApp } from './HooksApp.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { TodoApp } from "./08-useReducer/TodoApp";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <HooksApp /> */}
-    {/* <CounterApp /> */}
-    {/* <CounterWithCustomHook /> */}
-    {/* <SimpleForm /> */}
-    <FormWithCustomHook />
-  </React.StrictMode>,
-)
+    <TodoApp />
+  </React.StrictMode>
+);
